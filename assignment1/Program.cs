c8523a52f0f18947be72a875721e6036378539c4
chongleon <123053396+chongleon@users.noreply.github.com>
2024-03-05 22:18:46 +0800

    Add files via upload
    calculatorplus

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -1,25 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-//using System;
-class Program {
-    static void Main(String[] args)
+namespace Calculatorplus
+{
+    internal static class Program
     {
-        Console.WriteLine("请输入第一个参数。");
-        int num1 = int.Parse(Console.ReadLine());
-        //String number1=Console.ReadLine();
-        //int num1 = int.Parse(number1);
-        Console.WriteLine("请输入第二个参数。");
-        String number2 = Console.ReadLine();
-        int num2 = int.Parse(number2);
-        Console.WriteLine("请输入“+”或“-”或“*”或“/”。");
-        String symbol = Console.ReadLine();
-        int result = 0;
-        if (symbol == "+") result = num1 + num2;
-        else if (symbol == "-") result = num1 - num2;
-        else if (symbol == "*") result = num1 * num2;
-        else if (symbol == "/") result = num1 / num2;
-        Console.WriteLine("结果是：");
-        Console.WriteLine(result);
-        Console.ReadKey();
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
     }
- }
-
+}
