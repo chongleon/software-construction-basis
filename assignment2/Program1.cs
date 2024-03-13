b9753f2d9d1e9d548edc0351481a31fbbad1ad3c
chongleon <123053396+chongleon@users.noreply.github.com>
2024-03-13 13:23:33 +0800

    Add files via upload

---
+++ assignment2/Program1.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//用“埃氏筛法”求2~ 100以内的素数。2~ 100以内的数，
+//先去掉2的倍数，再去掉3的倍数，再去掉4的倍数，
+//以此类推...最后剩下的就是素数
+
+namespace PrimeNumber
+{
+    class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i <= 100; i++)
+            {
+                bool flag = true;
+                for(int j = 2; i > j; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }                
+                }
+                if (flag)
+                {
+                    Console.WriteLine(i + "是素数");
+                }
+            }
+        }
+    }
+}
